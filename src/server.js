1882257a0832161b5cdcdf53fd0294f6eed900c7
kimseongchan-kr <fpel8800@gmail.com>
2021-07-07 21:20:11 +0900

    middleware practice

--- src/server.js
+++ src/server.js
@@ -4,10 +4,29 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("response test");
-});
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url == "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed");
+  next();
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", (req, res) => {
+  return res.send("<h1>response test!</h1>");
+});
+app.get("/protected", (req, res) => {
+  return res.send("protected PAGE");
+});
 app.get("/login", (req, res) => {
   return res.send("LOGIN PAGE");
 });
