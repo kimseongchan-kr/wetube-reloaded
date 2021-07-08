1736ff7b477aec19b0839fe3f63eebd646a50bff
kimseongchan-kr <fpel8800@gmail.com>
2021-07-08 21:44:00 +0900

    Router practice

--- src/server.js
+++ src/server.js
@@ -8,6 +8,22 @@ const logger = morgan("dev");
 
 app.use(logger);
 
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
