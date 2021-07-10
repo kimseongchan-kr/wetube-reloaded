880705d02b3114ea78f18afeee75cedd6fac3105
kimseongchan-kr <fpel8800@gmail.com>
2021-07-10 17:52:01 +0900

    pug // include, extends setting & practice

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,7 @@
 export const search = (req, res) => res.send("Search Video");
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.send(`Watch Video #${req.params.id}`);
-export const edit = (req, res) => res.send("Edit Video");
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home!" });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const remove = (req, res) => res.send("Remove Video");
 export const upload = (req, res) => res.send("Upload Video");
