fe2bd76e7888c85f432a9feac1bb7e4a0428431f
kimseongchan-kr <fpel8800@gmail.com>
2021-07-10 19:23:24 +0900

    Video Html Page Add

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,9 @@
-export const search = (req, res) => res.send("Search Video");
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home!" });
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const remove = (req, res) => res.send("Remove Video");
-export const upload = (req, res) => res.send("Upload Video");
+export const search = (req, res) =>
+  res.render("search", { pageTitle: "Search" });
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const remove = (req, res) =>
+  res.render("delete", { pageTitle: "Delete" });
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
