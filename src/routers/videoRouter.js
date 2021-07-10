efe2c55e5c0c2a0b18734f1e183fa74f005650a7
kimseongchan-kr <fpel8800@gmail.com>
2021-07-10 15:05:40 +0900

    pug setting & pug practice

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,9 @@ import { see, edit, remove, upload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
+videoRouter.get("/upload", upload);
 videoRouter.get("/:id", see);
 videoRouter.get("/:id/edit", edit);
 videoRouter.get("/:id/delete", remove);
-videoRouter.get("/upload", upload);
 
 export default videoRouter;
