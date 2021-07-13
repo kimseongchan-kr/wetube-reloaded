6b53618dd1be6f96af7d85b91188e9e0386aeefe
kimseongchan-kr <fpel8800@gmail.com>
2021-07-13 21:59:02 +0900

    video watch, edit prototype

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,37 +1,47 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    commentts: 2,
+    createdAt: "2 miuntes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "second Video",
+    rating: 5,
+    commentts: 2,
+    createdAt: "2 miuntes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "third Video",
+    rating: 5,
+    commentts: 2,
+    createdAt: "2 miuntes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const search = (req, res) =>
   res.render("search", { pageTitle: "Search" });
-export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      commentts: 2,
-      createdAt: "2 miuntes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "second Video",
-      rating: 5,
-      commentts: 2,
-      createdAt: "2 miuntes ago",
-      views: 59,
-      id: 2,
-    },
-    {
-      title: "third Video",
-      rating: 5,
-      commentts: 2,
-      createdAt: "2 miuntes ago",
-      views: 59,
-      id: 3,
-    },
-  ];
-  return res.render("home", { pageTitle: "Home", videos });
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", videos });
+
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+
+  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+
 export const remove = (req, res) =>
   res.render("delete", { pageTitle: "Delete" });
+
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { see, edit, remove, upload } from "../controllers/videoController";
+import { watch, edit, remove, upload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
+videoRouter.get("/:id", watch);
 videoRouter.get("/:id/edit", edit);
 videoRouter.get("/:id/delete", remove);
 
