795ba1bfe1d1eeeeb3727c98cf2f69f0c3753d7d
kimseongchan-kr <fpel8800@gmail.com>
2021-07-18 16:57:20 +0900

    edit page가 DB를 이용하도록 수정

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -13,7 +13,9 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
   const { title, hashtags, description } = req.body;
-  const hashtagEdit = hashtags.split(",").map((word) => `#${word.trim()}`);
+  const hashtagEdit = hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word.trim()}`));
   try {
     await Video.create({
       title,
@@ -32,17 +34,36 @@ export const postUpload = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  console.log(video);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video Not Found!" });
+  }
   return res.render("watch", { pageTitle: video.title, video });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video Not Found!" });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const hashtagEdit = hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word.trim()}`));
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video Not Found!" });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtagEdit,
+  });
+
   return res.redirect(`/videos/${id}`);
 };
